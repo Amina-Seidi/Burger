c41dbc969838302de59ef127294d3c7240a1cef5
Minty <amina.seidi@uconn.edu>
2020-09-14 22:50:09 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT (10)AUTO_INCREMENT,
+burger_name VARCHAR(30),
+devoured BOOLEAN default false,
+PRIMARY KEY (id)
+)
 
