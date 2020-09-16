adc77580b668d2c20257e3e3c14bf06753b9c286
Minty <amina.seidi@uconn.edu>
2020-09-16 00:37:28 -0400

    Added code

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-const express = require("express")
+const express = require("express");
+
+// Create an instance of the express app.
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+//Parse the request as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server is now listening on PORT " + PORT);
+});
